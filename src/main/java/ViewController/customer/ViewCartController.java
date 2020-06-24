93088b81dd0db708bf0b178ac6a94f60705a6183
aminaryan80 <aryanmohammadamin@gmail.com>
2020-06-24 16:30:31 +0430

    View cart fixed.

---
+++ src/main/java/ViewController/customer/ViewCartController.java
@@ -0,0 +1,8 @@
+package ViewController.customer;
+
+import ViewController.Controller;
+import javafx.scene.layout.VBox;
+
+public class ViewCartController extends Controller {
+    public VBox vBox;
+}
